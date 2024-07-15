c380039636a16d958ddb1251fb1a1abbb8c9042d
Danilo Oliveira <dohkoweeds74@gmail.com>
2024-07-15 12:33:46 -0300

    Update: lowercase img names

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -131,7 +131,7 @@
                 <!-- Project 2 -->
                 <h3 class="work__project-title">Sistema de Gerenciamento de Bolsistas - SGB</h3>
                 <div class="work__img-wrapper work__image2 bording__bottom">
-                  <img class="lazy loading" src="/SGB.png" 
+                  <img class="lazy loading" src="/sgb.png" 
                     alt="Site de sistema de gerenciamento" />
                 </div>
                 <div class="work__project work__content2">
@@ -208,7 +208,7 @@
                 <!-- Project 6 -->
                 <h3 class="work__project-title">Sistema de Gerenciamento de Documentos</h3>
                 <div class="work__img-wrapper work__image6 bording__bottom">
-                  <img class="lazy loading" src="/BD.png"
+                  <img class="lazy loading" src="/bd.png"
                     alt="Sistema de Gerenciamento de Documentos" />
                 </div>
                 <div class="work__project work__content6">
@@ -226,7 +226,7 @@
                 <!-- Project 7 -->
                 <h3 class="work__project-title">PokeVi</h3>
                 <div class="work__img-wrapper work__image7 bording__top">
-                  <img class="lazy loading" src="/Pokevi.png"
+                  <img class="lazy loading" src="/pokevi.png"
                     alt="Pokedéx da 1ª e 2ª geração" />
                 </div>
                 <div class="work__project work__content7">
@@ -259,7 +259,7 @@
               <!-- Project 8 -->
               <h3 class="work__project-title">Fusion Attack!</h3>
               <div class="work__img-wrapper work__image8 bording__top">
-                <img class="lazy loading" src="/fusionattack.png" data-src="/fusionattack.png"
+                <img class="lazy loading" src="/fusionattack.png"
                   alt="Jogo de luta com dois personagens em cada lado da tela" />
               </div>
               <div class="work__project work__content8">
@@ -283,7 +283,7 @@
               <!-- Project 9 -->
               <h3 class="work__project-title">PokiAdventures</h3>
               <div class="work__img-wrapper work__image9 bording__bottom">
-                <img class="lazy loading" src="/pokiMO.png" data-src="/pokiMO.png"
+                <img class="lazy loading" src="/pokiadventures.png"
                   alt="Jogo de aventura estilo pokemon" />
               </div>
               <div class="work__project work__content9">
